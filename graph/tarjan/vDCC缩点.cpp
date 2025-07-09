dc91196a1653c86b5b4fcec48e8e43aac7c2f8b4
KakaZL <1340140310@qq.com>
2025-07-10 00:31:15 +0800

    m

--- graph/tarjan/vDCC缩点.cpp
+++ graph/tarjan/vDCC缩点.cpp
@@ -5,70 +5,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 1e5+5;
-vector<int> e[N],ne[N];
+int n,m,a,b;
+vector<int> e[N],ne[N]; 
 int dfn[N],low[N],tot;
 stack<int> stk;
+bool cut[N];
 vector<int> dcc[N];
-int cut[N],root,cnt,num,id[N];
+int root,cnt,num,id[N];
 
 void tarjan(int x){
-    dfn[x] = low[x] = ++tot;
+    dfn[x]=low[x]=++tot;
     stk.push(x);
-    if(!e[x].size()){
+    if(x==root&&!e[x].size()){//孤立点
         dcc[++cnt].push_back(x);
         return;
     }
-    int child = 0;
-    for (auto y : e[x]){
-        tarjan(y);
-        low[x] = min(low[x],low[y]);
-        if(low[y] >= dfn[x]){
-            child++;
-            if(x != root || child > 1){
-                cut[x] = 1;
+    int child=0;  
+    for(int y : e[x]){
+        if(!dfn[y]){//若y尚未访问
+            tarjan(y);////
+            low[x]=min(low[x],low[y]); 
+            if(low[y]>=dfn[x]){
+                child++;
+                if(x!=root||child>1){
+                    cut[x]=true; 
+                }
+                
+                int z; cnt++;
+
+                do{ //记录vDCC
+                    z = stk.top();
+                    dcc[cnt].push_back(z);
+                }while(z!=y);
+
+                dcc[cnt].push_back(x);
             }
-            cnt++;
-            while(1){
-                int z = stk.top();stk.pop();
-                dcc[cnt].push_back(z);
-                if(z == y) break;
-            }
-        }else{
-            low[x] = min(low[x],low[y]);
         }
+        else low[x]=min(low[x],dfn[y]);
     }
 }
-
 int main(){
-    int n,m;
-    cin >> n >> m;
-    while(m--){
-        int a,b;cin >> a >> b;
-        e[a].push_back(b);
+    cin>>n>>m;
+    while(m --){
+        cin>>a>>b;
+        e[a].push_back(b),
         e[b].push_back(a);
     }
-    for (root = 1;root <= n;root++){
+    for(root=1;root<=n;root++){
         if(!dfn[root]){
             tarjan(root);
         }
     }
-
-    //割点新编号
-    num = cnt;
-
-    for (int i = 1;i <= n;i++){
-        if(cut[i]) id[i] = ++num;
+        
+    //给每个割点一个新编号（cnt+1开始）
+    num=cnt;
+    for(int i=1;i<=n;i++){
+        if(cut[i])id[i]=++num;
     }
-
-    //建新图
-
-    for (int i = 1;i <= cnt;i++){
-        for (int j = 0;j < dcc[i].size();j++){
-            int x = dcc[i][j];
-            if(cut[x]){
-                ne[i].push_back(id[x]);
-                ne[id[x]].push_back(i);
-            }
+    //建新图，从每个vDCC向对应割点连边
+    for(int i=1;i<=cnt;i++){
+        for(int j=0;j<dcc[i].size();j++){
+            int x=dcc[i][j];
+                if(cut[x]){
+                    ne[i].push_back(id[x]),
+                    ne[id[x]].push_back(i);        
+                }
         }
     }
+    return 0;
 }
